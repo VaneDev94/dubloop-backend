155a22bdde67ce05ffa507c5e39dcbd11007e550
Vanessa Montero <vane@Mac.home>
2025-07-31 20:14:14 +0200

    Actualizacion del init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,7 +3,7 @@ from app.models.user import User
 from app.models.voice import VoiceProfile
 from app.models.metrics import JobMetrics
 from app.models.subtitle import Subtitle
-from elevenlabs import generate
+
 
 __all__ = [
     "Job",
