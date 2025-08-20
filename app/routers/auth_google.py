8eeda66cf151b64dd3468055f8498ed226231205
Vanessa Montero <vane@Mac.home>
2025-08-20 17:48:47 +0200

    Debug: añade print al callback de Google

--- app/routers/auth_google.py
+++ app/routers/auth_google.py
@@ -13,6 +13,7 @@ async def login(request: Request):
 
 @router.get("/callback")
 async def auth_google_callback(request: Request):
+    print("✅ LLEGÓ AL CALLBACK DE GOOGLE ✅")
     token = await oauth.google.authorize_access_token(request)
     user_info = await oauth.google.parse_id_token(request, token)
     user = await get_or_create_google_user(user_info)
